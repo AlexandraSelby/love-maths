2c11b3e77b8a53d2bbbbc0abaa3660ae0510dba5
AlexandraSelby <dagnaa19@gmail.com>
2025-07-17 11:38:17 +0100

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+function runGame(){
+
+}
+
+function checkAnswer() {
+
+}
+
+function caculateCorrectAnswer(){
+
+}
+
+function incrementScote() {
+
+}
+ function incrementWrongAnswer() {
+
+ }
+
+ function displayAdditionQuestion() {
+
+ }
+
+ function displaySubstractQuestion() {
+
+ }
+ function displayMultiplyQuestion() {
+
+ }
