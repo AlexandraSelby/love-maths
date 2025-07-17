c3136f259e82d0a147439494dd0a9bfe93f1a39f
AlexandraSelby <dagnaa19@gmail.com>
2025-07-17 16:50:06 +0100

    get buttons function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const buttons = document.getElementsByTagName("button");
+  for (let button of buttons) {
+    button.addEventListener("click", function() {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked Submit!");
+      } else {
+        let gameType = (this.getAttribute("data-type"));
+        alert(`You clicked ${gameType}`);
+      }
+    });
+  }
+});
+
 function runGame(){
 
 }
@@ -24,6 +38,6 @@ function incrementScote() {
  function displaySubstractQuestion() {
 
  }
- function displayMultiplyQuestion() {
+ function displayMultiplygQuestion() {
 
  }
